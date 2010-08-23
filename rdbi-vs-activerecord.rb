6e43e65fa485c7fa09acac28b20fe7c80ad3483b
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-08-22 21:05:07 -0400

    Our first benchmark.  Compare vanilla record INSERTion.

--- rdbi-vs-activerecord.rb
+++ rdbi-vs-activerecord.rb
@@ -1,5 +1,7 @@
-# require 'better-benchmark'
+require 'better-benchmark'
 require 'active_record'
+require 'rdbi'
+require 'rdbi/driver/postgresql'
 
 require_relative 'models/activerecord/record'
 
@@ -19,11 +21,26 @@ class RubyDatabaseBenchmark
       :password => DB_PASSWORD,
       :database => DB_DATABASE
     )
+
+    @dbh = RDBI.connect( :PostgreSQL, database: DB_DATABASE, username: DB_USER, password: DB_PASSWORD )
   end
 
   def run
-    t = Record.new( s: 'a string' )
-    t.save
+    result = Benchmark.compare_realtime(
+      :iterations => 10,
+      :inner_iterations => 2000,
+      :verbose => true
+    ) { |iteration|
+      s = 'a string'
+      t = Record.new( s: s )
+      t.save
+    }.with { |iteration|
+      s = 'a string'
+      @dbh.execute( "INSERT INTO records ( s ) VALUES ( ? )", s )
+    }
+
+    Benchmark.report_on result
+
   end
 end
 
